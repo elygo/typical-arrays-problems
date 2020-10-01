9f9695ed5f570932e72a8a42cdaf8613f3ad5cde
elygo <el.uzb93@gmail.com>
2020-10-01 12:12:50 +0500

    feat: add functions min, max, avg
    The required functions are submitted and tested.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,42 @@
 
 exports.min = function min (array) {
-  return 0;
+  if(array === undefined || array.length === 0){
+    return 0;
+  }
+  var minval = 1000;
+  for (var i=0; i < array.length; i++){
+    if(array[i]<minval){
+      minval = array[i]
+    } else {
+
+    }
+  }
+  return minval;
 }
 
 exports.max = function max (array) {
-  return 0;
+  if(array === undefined || array.length === 0){
+    return 0;
+  };
+  var maxval = 0;
+  for(var i=0; i < array.length; i++){
+    if (array[i] > maxval) {
+      maxval = array[i];
+    }
+    else {
+
+    }
+  }
+  return maxval;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if(array === undefined || array.length === 0){
+    return 0;
+  };
+  var total = 0;
+  for (var i=0; i < array.length; i++){
+      total = total + array[i];
+  }
+  return total/array.length;
 }
